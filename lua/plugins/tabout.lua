ad5fe052af747c8ab554483ebd0ce7b20c4198e6
Muhammad-Hassaan-Farooq <101121986+Muhammad-Hassaan-Farooq@users.noreply.github.com>
2024-09-17 15:40:10 +0500

    added tabout plugin

---
+++ lua/plugins/tabout.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    'abecodes/tabout.nvim',
+    lazy = false,
+    config = function()
+      require('tabout').setup {
+        tabkey = '<Tab>', -- key to trigger tabout, set to an empty string to disable
+        backwards_tabkey = '<S-Tab>', -- key to trigger backwards tabout, set to an empty string to disable
+        act_as_tab = true, -- shift content if tab out is not possible
+        act_as_shift_tab = false, -- reverse shift content if tab out is not possible
+        default_tab = '<C-t>', -- shift default action (only at the beginning of a line, otherwise <TAB> is used)
+        default_shift_tab = '<C-d>', -- reverse shift default action,
+        enable_backwards = true,
+        completion = false, -- disabled since you don't use nvim-cmp
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = '`', close = '`' },
+          { open = '(', close = ')' },
+          { open = '[', close = ']' },
+          { open = '{', close = '}' }
+        },
+        ignore_beginning = true,
+        exclude = {}
+      }
+    end,
+    dependencies = { "nvim-treesitter/nvim-treesitter" }, -- Keep only relevant dependencies
+  }
+}
+
+
